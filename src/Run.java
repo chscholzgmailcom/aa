5d1769560152cc24e302175e521319e3776e9439
christianscholz <christianscholz@christians-mbp.fritz.box>
2016-10-24 23:28:03 +0200

    The fourth line

--- src/Run.java
+++ src/Run.java
@@ -2,12 +2,10 @@
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 			System.out.println("Test me with commit  nh!");
 			System.out.println("This is the second line Test !");
 			System.out.println("And this is the third line !!");
-			
-			
+			System.out.println("Next test !");
 	}
 
 }
