40f0fdcec893e89b135c98677793cf5fdf6b79d9
christianscholz <christianscholz@christians-mbp.fritz.box>
2016-10-24 23:21:33 +0200

    This is a first real test with GIT

---
+++ src/Run.java
@@ -0,0 +1,11 @@
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Test me with commit  nh!");
+			System.out.println("This is the second line Test !");
+			
+	}
+
+}
